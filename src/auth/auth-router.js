574725036c7e90f8a5f23247dcc81bdbb1a64fbc
bix <bixmail6@gmail.com>
2020-09-18 09:53:09 -0700

    add token refresh

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const AuthService = require("./auth-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
@@ -39,6 +40,14 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
       });
     })
     .catch(next);
+});
+
+authRouter.post("/refresh", requireAuth, (req, res) => {
+  const sub = req.user.user_name;
+  const payload = { user_id: req.user.id };
+  res.send({
+    authToken: AuthService.createJwt(sub, payload),
+  });
 });
 
 module.exports = authRouter;
